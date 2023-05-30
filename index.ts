8ac16a28a3ee51b3e858cf67e88f565aa60ac024
henryjw <3443648+henryjw@users.noreply.github.com>
2023-05-30 17:35:44 -0400

    add function to get battery life + some cleanup

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import noble from "@abandonware/noble";
+import noble, { Peripheral } from "@abandonware/noble";
 
 (async () => {
     try {
@@ -10,52 +10,74 @@ import noble from "@abandonware/noble";
     }
 
     noble.on("discover", async (device) => {
-        const minimalDevice = {
-            name: device.advertisement.localName,
-            connectable: device.connectable,
-            state: device.state
-        };
+        const minimalDevice = toMinimalDevice(device);
+
+        const deviceName = minimalDevice.name;
 
 
-        if (!minimalDevice.name) {
-            return
+        if (!deviceName) {
+            return;
         }
 
-        const isMakeBlockDevice = minimalDevice.name.toLowerCase().startsWith("makeblock");
+        const isMakeBlockDevice = deviceName.toLowerCase().startsWith("makeblock");
 
         if (!isMakeBlockDevice) {
-            console.log("Found non-MakeBlock device", minimalDevice.name);
+            console.log("Found non-MakeBlock device", deviceName);
             return;
         }
 
         await noble.stopScanningAsync();
-        console.log("Found MakeBlock device", minimalDevice.name);
+        console.log("Found MakeBlock device", deviceName);
         console.log("Connecting to MakeBlock device...");
         await device.connectAsync();
         console.log("Connected to MakeBlock device");
 
-        // const {characteristics} = await device.discoverSomeServicesAndCharacteristicsAsync(['180f'], ['2a19']);
-        // const batteryLevel = (await characteristics[0].readAsync())[0];
-        const { characteristics, services } = await device.discoverAllServicesAndCharacteristicsAsync();
+        await wait(10_000);
 
-        // console.log("Services", prettyPrint(services));
-        // console.log(`${device.address} (${device.advertisement.localName}): ${batteryLevel}%`);
+        console.log("Exiting...");
+        await device.disconnectAsync();
 
-        console.log("Sending message to each characteristic...");
-        for (const characteristic of characteristics) {
-            try {
-                await characteristic.writeAsync(Buffer.from('test'), false);
-                console.log(`Sent message to characteristic with ID ${characteristic.uuid}`);
-            } catch(err) {
-                console.error(`Error sending message to characteristic with ID ${characteristic.uuid}`, err);
-            }
-        }
-
-        console.log("Sent message to all characteristics. Exiting...");
         process.exit(0);
     });
 })();
 
 function prettyPrint(object: any): void {
     console.log(JSON.stringify(object, null, 2))
+}
+
+/**
+ * Get the battery level of a device. Returns null if unable to get the battery level.
+ * @param device
+ */
+async function getDeviceBatteryLevel(device: Peripheral): Promise<number | null> {
+    if (!device.connectable) {
+        console.log("Device is not connectable");
+        return null;
+    }
+
+    const minimalDevice = toMinimalDevice(device);
+
+    console.debug(`Connecting to device "${minimalDevice.name}" ...`)
+
+    console.debug(`Connected to device "${minimalDevice.name}"`)
+
+    const {characteristics, services} = await device.discoverAllServicesAndCharacteristicsAsync();
+
+    await device.disconnectAsync();
+
+    const batteryLevelCharacteristic = characteristics.find((characteristic: any) => characteristic.uuid.toUpperCase() === '2A19');
+    const batteryLevel: Buffer | null = await batteryLevelCharacteristic?.readAsync() ?? null;
+    return batteryLevel?.readUInt8(0) ?? null;
+}
+
+function toMinimalDevice(device: Peripheral) {
+    return {
+        name: device.advertisement.localName,
+        connectable: device.connectable,
+        state: device.state,
+    };
+}
+
+async function wait(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
